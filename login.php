1438322b1b13caaeb522e3048931c660d847d827
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-02 03:27:00 -0800

    this is login page which contains the email and password field

---
+++ login.php
@@ -0,0 +1,8 @@
+<form method="post" action="login_retrieve.php">
+Email-ID:<input type="email" name="email"><br><br>
+Password:<input type="password" name="pass"><br><br>
+<input type="submit" value="Login">
+
+
+
+</form>
