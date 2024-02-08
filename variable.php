6433f968b7b54af2d3c87c96c1b4f769af04a8ec
pramodsaini <pramodsaini25062000@gmail.com>
2024-02-08 02:09:19 -0800

    variable in php

---
+++ variable.php
@@ -0,0 +1,36 @@
+<?php
+
+$i=5;
+ echo $i;//5
+
+?>
+<?php
+
+
+$i=500;
+
+ echo "$i";
+?>
+<?php
+
+  $x="John";
+
+  echo 'Hello $x';
+
+   
+
+?>
+<?php
+  $x="John";
+
+  echo "Hello $x";
+
+  ?>
+
+  <?php
+  $i=5;
+  $j="i";
+
+  echo $$j;
+
+  ?>
